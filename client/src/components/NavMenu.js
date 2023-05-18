95c4e3279a9b35776d93ab0abb005c7366274094
Julianne <juliaguas17@gmail.com>
2023-05-17 20:41:40 -0500

    added NavMenu

---
+++ client/src/components/NavMenu.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+// import global styling
+
+export default function NavMenu() {
+  return (
+    <div>NavBar</div>
+  )
+}
