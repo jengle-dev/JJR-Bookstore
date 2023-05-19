7aff0c7972a780ed3e8812a73b62260f26bada8a
Julianne <juliaguas17@gmail.com>
2023-05-18 20:34:55 -0500

    added SearchBar with styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; //used to route navMenu components
+import { BrowserRouter as Router, Route, Route } from 'react-router-dom'; //used to route navMenu components
 
 import NavMenu from './components/NavMenu';
 import Home from './pages/Home';
@@ -22,17 +22,22 @@ const client = new ApolloClient({
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-    {/* not sure if apollo needs to wrap around Chakra */}
-    <ChakraProvider>
-      // update class div to Chakra header class
-      <div className="App">
-        <header className="App-header">
-         {/* the components and routes to different pages */}
-        </header>
-      </div>
-    </ChakraProvider>
-    </ApolloProvider>
+    <Router>
+      <ApolloProvider client={client}>
+      {/* not sure if apollo needs to wrap around Chakra -- do Chakra and Apollo go in index.js or App.js? */}
+        <ChakraProvider>
+          <div className="App">
+            <Routes>
+              {/* the components and routes to different pages */}
+              <Route
+                path="/"
+                element={<Home />}
+                />
+            </Routes>
+          </div>
+        </ChakraProvider>
+      </ApolloProvider>
+    </Router>
   );
 }
 
--- client/src/components/NavMenu.js
+++ client/src/components/NavMenu.js
@@ -3,6 +3,8 @@ import { NavLink } from 'react-router-dom';
 import './index.css'; // import global styling
 import styled from 'styled-components'; // imports from package.json dependencies
 
+
+
 export default function NavMenu() {
   return (
     <div>NavBar</div>
