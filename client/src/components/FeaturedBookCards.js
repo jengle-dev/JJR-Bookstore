13dbc927790b4824852f7bcca3bbe40b33874e19
jengle-dev <j.engle.dev@gmail.com>
2023-05-18 20:26:18 -0500

    adding files for cards and component, carousel and server.js

--- client/src/components/FeaturedBookCards.js
+++ client/src/components/FeaturedBookCards.js
@@ -9,51 +9,25 @@ import {
     chakra,
     Tooltip,
   } from '@chakra-ui/react';
-  import { BsStar, BsStarFill, BsStarHalf } from 'react-icons/bs';
   import { FiShoppingCart } from 'react-icons/fi';
   
   const data = {
-    isNew: true,
+    [
+    {isNew: true,
     imageURL:
-      'https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80',
-    name: 'Wayfarer Classic',
-    price: 4.5,
-    rating: 4.2,
-    numReviews: 34,
+      'client/public/featuredBooks/1984.jpg',
+    name: 'Nineteen-Eighty-Four',
+    author: 'George Orwell',
+    price: 7.5,
+    isbn: ''},
+    {
+
+,    }
+  ]
+  
   };
   
-  interface RatingProps {
-    rating: number;
-    numReviews: number;
-  }
   
-  function Rating({ rating, numReviews }: RatingProps) {
-    return (
-      <Box d="flex" alignItems="center">
-        {Array(5)
-          .fill('')
-          .map((_, i) => {
-            const roundedRating = Math.round(rating * 2) / 2;
-            if (roundedRating - i >= 1) {
-              return (
-                <BsStarFill
-                  key={i}
-                  style={{ marginLeft: '1' }}
-                  color={i < rating ? 'teal.500' : 'gray.300'}
-                />
-              );
-            }
-            if (roundedRating - i === 0.5) {
-              return <BsStarHalf key={i} style={{ marginLeft: '1' }} />;
-            }
-            return <BsStar key={i} style={{ marginLeft: '1' }} />;
-          })}
-        <Box as="span" ml="2" color="gray.600" fontSize="sm">
-          {numReviews} review{numReviews > 1 && 's'}
-        </Box>
-      </Box>
-    );
-  }
   
   function ProductAddToCart() {
     return (
