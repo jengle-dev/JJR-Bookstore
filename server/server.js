4cdfa0c6c5f2439d06580069c99ad76d4381340a
Rebecca Lehew <lehew316@gmail.com>
2023-05-19 20:32:37 -0500

    Updates to try to solve server errors

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
