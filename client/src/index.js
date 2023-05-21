18b36131ddc49847ada2a3427ebf4a5b0d9f2c15
Julianne <juliaguas17@gmail.com>
2023-05-21 17:00:04 -0500

    fixed server header error

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
 import App from './App';
 //import reportWebVitals from './reportWebVitals';
@@ -7,7 +8,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>
 );
 
