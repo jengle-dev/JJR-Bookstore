7de81ae6dbb8abad2a4d44b5d11d1068c285b5f4
Julianne <juliaguas17@gmail.com>
2023-05-22 17:09:53 -0500

    changed

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 //import { useQuery } from '@apollo/client';
 
+// background color: var(--ecruPrincess) -- do we like this as a general background color?
+
 const Home = () => {
   return (
     <div>
