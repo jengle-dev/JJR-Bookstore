3b765bb8734ae475f976339ef35403009a56f892
Julianne <juliaguas17@gmail.com>
2023-05-22 16:37:37 -0500

    added about us section

--- client/src/components/NavMenu.js
+++ client/src/components/NavMenu.js
@@ -10,10 +10,10 @@ const NavStyles = styled.nav`
   left: 0;
   width: 100%;
   padding: 1rem 0;
-  background-color: var(--muddyRiver);
+  background-color: var(--mossyRock);
   color: var(--ecruPrincess);
   ul {
-    font-family: 'Lora, serif';
+    font-family: 'Lora';
     font-weight: 700;
     max-width: 1200px;
     margin: 0 auto;
@@ -40,8 +40,8 @@ const NavStyles = styled.nav`
     }
     .active {
       border-radius: 8px;
-      background-color: var(--mossyRock);
-      color: var(--blush);
+      background-color: var(--muddyRiver);
+      color: var(--taupeToad);
     }
   }
   .mobile-menu-icon {
@@ -70,7 +70,7 @@ const NavStyles = styled.nav`
     .navItems {
       --top: 1.2rem;
       transition: 0.3s ease transform;
-      background-color: var(--ecruPrincess);
+      background-color: var(--mossyRock);
       padding: 0.2rem 0.2rem 2rem 0rem;
       width: 70%;
       max-width: 300px;
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,13 +1,83 @@
 import React from 'react';
-//import { useQuery } from '@apollo/client';
+import { Text, Image } from '@chakra-ui/react';
+import styled from 'styled-components';
+import AboutImg from '../assets/images/about-us-img.png';
+
+const AboutPageStyles = styled.div`
+  padding: 8rem 0 8rem 0; 
+  background-color: var(--taupeToad);
+  .top-section {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8rem;
+  }
+  .left {
+    flex: 4;
+  }
+  .right {
+    flex: 4;
+  }
+  .about__subheading {
+    margin-top: 2rem;
+    font-size: 2rem;
+    
+    span {
+      padding: 0.5rem;
+      border-radius: 8px;
+    }
+  }
+  .about__heading {
+    font-size: 5rem;
+    
+    color: var(--oliveCoat);
+  }
+  .about__info {
+    font-size: 1.2rem;
+    margin-bottom: 2rem;
+    .para {
+      max-width: 100%;
+    }
+  }
+  @media only screen and (max-width: 768px) {
+    padding: 10rem 0;
+    .top-section {
+      flex-direction: column;
+      gap: 5rem;
+    }
+    .about__subheading {
+      font-size: 2rem;
+    }
+    .about__heading {
+      font-size: 3rem;
+    }
+  }
+`;
 
 
-const About = () => {
-  return (
-    <div>
-      <div>About Us</div>
-    </div>
-  )
-}
-
-export default About;
+export default function About() {
+    return (
+      <>
+      <AboutPageStyles>
+        <div className='container'>
+          <div className='top-section'>
+            <div className='left'>
+              <p className='about__subheading'>
+                Meet Jennifer, Julianne and Rebecca!
+              </p>
+              <h2 className="about__heading">About the Owners</h2>
+              <div className="about__info">
+                <Text fontSize='2xl'>
+                During KU's full-stack coding bootcamp, we discovered a passion for literature and embarked on a daring adventure to open an online community for booklovers. Our journey serves as a testament to the transformative power of friendship, dedication, and the pursuit of dreams.
+                </Text>
+              </div>
+            </div>
+            <div className="right">
+              <Image boxSize="lg" src={AboutImg} alt="about us img" />
+            </div>
+          </div>
+        </div>
+      </AboutPageStyles>
+      </>
+    );
+  }
