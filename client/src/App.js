ea2c7ad106875802d157f3120ee8f17a3dc8a25f
jengle-dev <j.engle.dev@gmail.com>
2023-05-22 20:12:23 -0500

    update to footer and trying to style chakra

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; //used to route navMenu components
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
@@ -8,23 +8,22 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 //import Search from './pages/SearchBook';
 import NavMenu from './components/NavMenu';
-//import Footer from './components/Footer';
-// 1. Import `extendTheme`
-import { extendTheme } from "@chakra-ui/react"
+import Footer from './components/Footer';
 
-// 2. Call `extendTheme` and pass your custom values
-const theme = extendTheme({
-  colors: {
-   mainColosr: {
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
     ivoryGoddess: "#F5EBE0",
     ecruPrincess: "#E3D5CA",
     taupeToad: "#D5BDAF",
     mossyRock: "#79836A",
     oliveCoat: "#586445",
-    muddyRiver: "#4E443A"
-    }
-  }
-})
+    muddyRiver: "#4E443A",
+    licorice: "#221508",
+  },
+}
+
+const theme = extendTheme({ colors })
 // Home will include the following components...
 // Navigation/Header
 // Carousel/Footer
@@ -41,22 +40,22 @@ function App() {
   return (
     <Router>
       <ApolloProvider client={client}>
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
           <div className="App">
             <NavMenu />
-              <Switch>
-                <Route path="/about">
-                  <About />
-                </Route>
-                <Route path="/contact">
-                  <Contact />
-                </Route>
-                {/* Search? */}
-                <Route path="/">
-                  <Home />
-                </Route>
-              </Switch>
-            {/* Footer */}
+            <Switch>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/contact">
+                <Contact />
+              </Route>
+              {/* Search? */}
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+              <Footer />
           </div>
         </ChakraProvider>
       </ApolloProvider>
