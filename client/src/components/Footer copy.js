8ddcb797453c7baed96f97a4b814dee7335fdc4f
jengle-dev <j.engle.dev@gmail.com>
2023-05-24 17:29:46 -0500

    commented out unused component pages

--- client/src/components/Footer copy.js
+++ client/src/components/Footer copy.js
@@ -1,24 +1,24 @@
-import * as React from 'react';
+// import * as React from 'react';
 
-import { Flex, Text, Box, HStack, Circle, IconButton, Container } from '@chakra-ui/react';
+// import { Flex, Text, Box, HStack, Circle, IconButton, Container } from '@chakra-ui/react';
 
-// export default Footer;
-export default function Footer() {
-  // const [showFooter, setFooter] = useState(false);
-  return (
-    <Flex as="footer" className='footer' >
-      <HStack spacing="24px" align="center" />
-      <Flex gap="10px" wrap="wrap" justify="center">
-        <Box as="div">
-          <IconButton aria-label='contact-us' icon={<Circle />}/><Text textColor="#E3D5CA" bg={'oliveCoat.900'}>Contact Us</Text>
-        </Box>
-        <Box as="div">
-          <IconButton aria-label='about-us' bg={'oliveCoat.900'} icon={<Circle />}/><Text as ="h3" >About Us</Text>
-        </Box>
-        <Box as="div">
-          <IconButton aria-label='about-us' icon={<Circle />}/><Text textColor="muddyRiver.900">Address</Text>
-        </Box>
-      </Flex>
-   </Flex>
-  )
-}
+// // export default Footer;
+// export default function Footer() {
+//   // const [showFooter, setFooter] = useState(false);
+//   return (
+//     <Flex as="footer" className='footer' >
+//       <HStack spacing="24px" align="center" />
+//       <Flex gap="10px" wrap="wrap" justify="center">
+//         <Box as="div">
+//           <IconButton aria-label='contact-us' icon={<Circle />}/><Text textColor="#E3D5CA" bg={'oliveCoat.900'}>Contact Us</Text>
+//         </Box>
+//         <Box as="div">
+//           <IconButton aria-label='about-us' bg={'oliveCoat.900'} icon={<Circle />}/><Text as ="h3" >About Us</Text>
+//         </Box>
+//         <Box as="div">
+//           <IconButton aria-label='about-us' icon={<Circle />}/><Text textColor="muddyRiver.900">Address</Text>
+//         </Box>
+//       </Flex>
+//    </Flex>
+//   )
+// }
